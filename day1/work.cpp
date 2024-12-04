74fcf1afa59d3621d79e1e7459ae2ddd983868be
Angelo Fiataruolo <angelo3fiat@gmail.com>
2024-12-04 10:54:14 -0600

    Forgot to commit changes yesterday?

--- day1/work.cpp
+++ day1/work.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main() {
+void p1() {
     ifstream f("data.txt");
 
     int a[1000], b[1000];
@@ -24,9 +24,47 @@ int main() {
     for (int i = 0; i < 1000; i++) 
         sum += abs(a[i] - b[i]);
 
-    printf("Sum: %d\n", sum);
+    printf("Sum of differences: %d\n", sum);
 
     f.close();
+}
 
-    return 0;
+void p2() {
+    ifstream f("data.txt");
+
+    int a[1000], b[1000];
+
+    for (int i = 0; i < 1000; i++) {
+        f >> a[i];
+        f >> b[i];
+    }
+
+    int n = sizeof(a) / sizeof(a[0]);
+
+    sort(a, a + n);
+    sort(b, b + n);
+
+    int counts[1000];
+    int start = 0, j = 0;
+
+    for(int i = 0; i < 1000; i++) {
+        counts[i] = 0;
+        while(b[start] < a[i]) start++;
+        while(b[start + j] == a[i]) {
+            j++;
+            counts[i]++;
+        }
+        j = 0;
+    }
+
+    int sum = 0;
+
+    for(int i = 0; i < 1000; i++) sum += a[i] * counts[i];
+
+    printf("Similarity score: %d\n", sum);
+}
+
+int main() {
+    p1();
+    p2();
 }
