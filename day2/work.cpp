5f6dfe2b98e347a99e3bb9f75a13f4d06bb5b327
Angelo Fiataruolo <angelo3fiat@gmail.com>
2024-12-13 16:17:55 -0600

    Initial attempt at day 2, not working yet

--- day2/work.cpp
+++ day2/work.cpp
@@ -4,8 +4,37 @@
 
 using namespace std;
 
+int signOf(int x) {
+    return (x > 0) - (x < 0);
+}
+
+bool isSafe(int row[]) {
+    int diff = row[0] - row[1];
+    int dir = signOf(diff);
+    bool safe = true;
+
+    for(int j = 0; j < 4; j++) {
+        diff = row[j] - row[j+1];
+        if(signOf(diff) != dir || abs(diff) > 2) {
+            safe = false;
+            break; // bad practice...
+        }
+
+    }
+    return safe;
+}
 void p1() {
     ifstream f("data.txt");
+
+    int row[5], safeReports = 0;
+
+    for(int i = 0; i < 1000; i++) {
+        for(int j = 0; j < 5; j++) f >> row[j];
+
+        safeReports += isSafe(row);
+    }
+
+    printf("Safe Reports: %d\n", safeReports);
 
     f.close();
 }
@@ -18,5 +47,5 @@ void p2() {
 
 int main() {
     p1();
-    p2();
+    // p2();
 }
